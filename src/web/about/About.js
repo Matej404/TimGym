ecdd6d7772e36fc0aa402883337c71cde239373c
Matej404 <matejjurin278@gmail.com>
2022-03-23 13:57:30 +0000

    about section added photos

--- src/web/about/About.js
+++ src/web/about/About.js
@@ -1,7 +1,32 @@
 import "./about.css";
 import Gym from "./gym.jpg";
+import { imgData } from "./ImgData";
+import React, { useState } from "react";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-scroll";
 
 export default function About() {
+    const [popUp, setPopUp] = useState("");
+
+    const handlePopUp = (e, id) => {
+        setPopUp(id);
+    }
+
+    const handleRemovePopUp = () => {
+        setPopUp("")
+    }
+
+    //slide
+    const [current, setCurrent] = useState(0);
+
+    const prevBtn = () => {
+        setCurrent(current === 0 ? imgData.length - 1 : current - 1);
+    }
+
+    const nextBtn = () => {
+        setCurrent(current === imgData.length - 1 ? 0 : current + 1);
+    }
 
     return(
         <div className="about">
@@ -25,6 +50,40 @@ export default function About() {
                     </p>
                 </div>
             </div>
+            <Link to="photos" spy={true} smooth={true}>
+                <div className="view-photos">
+                    VIEW PHOTOS
+                </div>
+            </Link>
+            <div className="photos" id="photos">
+            <div className="photos-container">
+                {imgData.map((image, index) => {
+                    const {id, img} = image;
+                    return(
+                        <div key={id}>
+                            <div className="single-photo" onClick={(e) => handlePopUp(e, id)}>
+                                <img src={img} />
+                            </div>
+                            {popUp === id && (
+                                <div key={id} className="popUp-img">
+                                    <AiOutlineClose className="btn-close" onClick={() => handleRemovePopUp()} />
+                                    <RiArrowLeftSLine className="btn-left" onClick={prevBtn} />
+                                    <RiArrowRightSLine className="btn-right" onClick={nextBtn} />
+                                    {imgData.map((image, index) => (
+                                        <div key={index}>
+                                            {index === current && (
+                                                <img src={image.img} />
+                                            )}
+                                            
+                                        </div>
+                                    ))}
+                                </div>
+                            )}   
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
         </div>
     )
 }
---
+++ src/web/about/ImgData.js
@@ -0,0 +1,83 @@
+import gym1 from "./gym-photos/gym1.jpeg";
+import gym2 from "./gym-photos/gym2.jpeg";
+import gym3 from "./gym-photos/gym3.jpeg";
+import gym4 from "./gym-photos/gym4.jpeg";
+import gym5 from "./gym-photos/gym5.jpeg";
+import gym6 from "./gym-photos/gym6.jpeg";
+import gym7 from "./gym-photos/gym7.jpeg";
+import gym8 from "./gym-photos/gym8.jpeg";
+import gym9 from "./gym-photos/gym9.jpeg";
+import gym10 from "./gym-photos/gym10.jpeg";
+import gym11 from "./gym-photos/gym11.jpeg";
+import gym12 from "./gym-photos/gym12.jpeg";
+import gym13 from "./gym-photos/gym13.jpeg";
+import gym14 from "./gym-photos/gym14.jpeg";
+import gym15 from "./gym-photos/gym15.jpeg";
+import gym16 from "./gym-photos/gym16.jpeg";
+
+export const imgData = [
+    {
+        id: 1,
+        img: gym1
+    },
+    {
+        id: 2,
+        img: gym2
+    },
+    {
+        id: 3,
+        img: gym3
+    },
+    {
+        id: 4,
+        img: gym4
+    },
+    {
+        id: 5,
+        img: gym5
+    },
+    {
+        id: 6,
+        img: gym6
+    },
+    {
+        id: 7,
+        img: gym7
+    },
+    {
+        id: 8,
+        img: gym8
+    },
+    {
+        id: 9,
+        img: gym9
+    },
+    {
+        id: 10,
+        img: gym10
+    },
+    {
+        id: 11,
+        img: gym11
+    },
+    {
+        id: 12,
+        img: gym12
+    },
+    {
+        id: 13,
+        img: gym13
+    },
+    {
+        id: 14,
+        img: gym14
+    },
+    {
+        id: 15,
+        img: gym15
+    },
+    {
+        id: 16,
+        img: gym16
+    }
+]
